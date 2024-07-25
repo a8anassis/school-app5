990341519d3ff4984dc74a42b7c281a4af01335f
Th. Androutsos <a8anassis@gmail.com>
2024-07-25 15:00:23 +0300

    model test

---
+++ src/test/java/gr/aueb/cf/schoolapp/model/TeacherTest.java
@@ -0,0 +1,29 @@
+package gr.aueb.cf.schoolapp.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TeacherTest {
+
+    @Test
+    void gettersSetters() {
+        Teacher teacher = new Teacher();
+        teacher.setId(1);
+        assertEquals(teacher.getId(), 1);
+        teacher.setFirstname("Thanassis");
+        assertEquals(teacher.getFirstname(), "Thanassis");
+        teacher.setLastname("Androutsos");
+        assertEquals(teacher.getLastname(), "Androutsos");
+    }
+
+    @Test
+    void overloadedConstructorAndToString() {
+        Teacher teacher = new Teacher(2, "Anna", "Giannoutsou");
+        assertEquals(teacher.getId(), 2);
+        assertEquals(teacher.getFirstname(), "Anna");
+        assertEquals(teacher.getLastname(), "Giannoutsou");
+        String expected = "2, Anna, Giannoutsou";
+        assertEquals(expected, teacher.toString());
+    }
+}
